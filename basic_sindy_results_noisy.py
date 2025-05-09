1e9b6b0b95a8f4aabd1a3f096d9b7ca620dda2b5
Brooks Howe <zbrookshowe@gmail.com>
2025-05-08 21:07:06 -0500

    added loop for looping through noise levels, still haven't decided if this should be a script or another short program for analysis.

--- basic_sindy_results_noisy.py
+++ basic_sindy_results_noisy.py
@@ -1,7 +1,7 @@
 '''
 File:         basic_sindy_results_noisy.py
 Written by:   Brooks Howe
-Last updated: 2025/05/01
+Last updated: 2025/05/08
 Description:  Python script which generates results using the SINDy algorithm for the 3-body Yukawa
     system of point particles with different noise levels added.
 '''
@@ -13,36 +13,40 @@ import pysindy as ps
 # define relative path to data directory
 data_dir = 'data/basic_noisy'
 # define noise levels
-noise_levels = [0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4]
+noise_levels = [1e-3, 5e-3, 1e-2]
 # define threshold values to use with SINDy
 threshold_array:np.ndarray = np.arange(0.0, 0.9, 0.1)
 
-# create list of sim objects with different noise levels
-seed_num = 109274
-rng = np.random.default_rng(seed=seed_num)
-sim_list = y3.multiple_simulate(duration=1e-1, n_trajectories=200, potential_type='repulsive', rng=rng, 
-                                save_data=False)
+# loop through noise levels
+for noise_level in noise_levels:
+    # create directory name for this specific noise level
+    data_dir_noise_level = data_dir + '/noise_level_' + str(noise_level)
+    # create list of sim objects with different noise levels
+    seed_num = 109274
+    rng = np.random.default_rng(seed=seed_num)
+    sim_list = y3.multiple_simulate(duration=1e-1, n_trajectories=200, potential_type='repulsive', rng=rng, 
+                                    save_data=True, directoryname=data_dir_noise_level)
 
-# transform to subtracted space and add noise to each simulation in sim_list
-x_train = []
-for sim in sim_list:
-    # subtract data and add noise
-    sim.subtract_data()
-    sim.add_gaussian_noise(noise_level=noise_levels[0]) # just use one noise level for now
-    # collect data into list x_train for SINDy fitting
-    x_train.append(sim.x)
+    # transform to subtracted space and add noise to each simulation in sim_list
+    x_train = []
+    for sim in sim_list:
+        # subtract data and add noise
+        sim.subtract_data()
+        sim.add_gaussian_noise(noise_level=noise_level) # just use one noise level for now
+        # collect data into list x_train for SINDy fitting
+        x_train.append(sim.x)
 
-# extract labels and time step from last simulation in sim_list
-x_train_labels = sim.labels
-dt = sim.dt
-# fit a SINDy model using different thresholds
-lib = y3.generate_3body_library()
-for threshold in threshold_array:
-    opt = ps.STLSQ(threshold=threshold)
-    model = ps.SINDy(optimizer=opt, feature_names=x_train_labels, feature_library=lib)
-    model.fit(x_train, t=dt, multiple_trajectories=True)
-    print('STLSQ threshold:', threshold)
-    print('Std. dev. of noise:', noise_levels[0])
-    print('complexity:', model.complexity)
-    model.print()
-    print('------------------------------')
+    # extract labels and time step from last simulation in sim_list
+    x_train_labels = sim.labels
+    dt = sim.dt
+    # fit a SINDy model using different thresholds
+    lib = y3.generate_3body_library()
+    for threshold in threshold_array:
+        opt = ps.STLSQ(threshold=threshold)
+        model = ps.SINDy(optimizer=opt, feature_names=x_train_labels, feature_library=lib)
+        model.fit(x_train, t=dt, multiple_trajectories=True)
+        # print('STLSQ threshold:', threshold)
+        # print('Std. dev. of noise:', noise_levels[0])
+        # print('complexity:', model.complexity)
+        y3.print_SINDy_nice(model)
+        print(100*'=')
