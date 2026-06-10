256df7b58924e708787d4426e63f4df93f0c53ea
Brooks Howe <zbrookshowe@gmail.com>
2026-06-10 16:12:35 -0500

    Deleted repeat of func '_EOM' which was used for testing, added func '_too_close' to stop solve_ivp before soln blows up, changed func 'simulate' to save the actual time simulated, fixed bug in 'plot' (lines 255,257), made plots prettier and included actual duration printed on plot. ipynb just changed for testing.

--- anisotropic_potential.py
+++ anisotropic_potential.py
@@ -143,55 +143,27 @@ class Anisotropic_simulation(ys.Simulation):
         
         alpha = 4 - np.pi
 
-        # this needs to be corrected, I don't think it is going to work
-        # P_2 = lambda x: legendre_p(2, x, diff_n=1)[0]
-        P_2 = lambda x: (1/2) * (3*x**2 - 1)
-        # P_2_prime = lambda x: legendre_p(2, x, diff_n=1)[1]
-        P_2_prime = lambda x: 3*x
+        P_2 = lambda x: legendre_p(2, x, diff_n=1)[0]
+        P_2_prime = lambda x: legendre_p(2, x, diff_n=1)[1]
+
+        # P_2 = lambda x: (1/2) * (3*x**2 - 1)  # redefined explicitly for testing
+        # P_2_prime = lambda x: 3*x             # same here
+        
 
         r_dot       = x[1]
         p_dot       = x[3]**2 / x[0]**3 + SCALING_CONST * np.exp( - x[0] ) / x[0]**2 + \
                         SCALING_CONST * np.exp( - x[0] ) / x[0] - \
-                        SCALING_CONST * 3 * alpha * MACH_NUM**2 * P_2( np.cos(x[2]) )/ x[0]**4
+                        SCALING_CONST * 3 * alpha * MACH_NUM**2 * P_2( np.cos(x[2]) ) / x[0]**4
         theta_dot   = x[3] / x[0]**2
         l_dot       = SCALING_CONST * alpha * MACH_NUM**2 * -np.sin(x[2]) * P_2_prime( np.cos(x[2]) ) / x[0]**3 
 
         return r_dot, p_dot, theta_dot, l_dot
     
-    # def _EOM(self, t, x):
-    #     '''
-    #     Description: Private method with the anisotropic, Hamiltonian equations of motion for a
-    #         two-body system in two dimensions. The anisotropic potential comes from the 
-    #         dissertation of R. Kompaneets, published in 2007: "Complex plasmas: Interaction 
-    #         potentials and non-Hamiltonian dynamics," pp. 37. The state vector of the system is
-    #         represented by a numpy array `x`, which has the form
-    #         x[0] = r (interptcl spacing), 
-    #         x[1] = p (linear momentum), 
-    #         x[2] = theta (angle wrt x-axis, or electric field direction), 
-    #         x[3] = l (angular momentum)
-    #         where r is the interparticle separation, p is its conjugate momentum, theta is the 
-    #         angle between the separation vector and the external electric field, and l is the 
-    #         angular momentum (momentum conjugate to theta).
-    #     '''
-    #     global SCALING_CONST
-    #     global MACH_NUM
-        
-    #     alpha = 4 - np.pi
-
-    #     # this needs to be corrected, I don't think it is going to work
-    #     # P_2 = lambda x: legendre_p(2, x, diff_n=1)[0]
-    #     P_2 = lambda x: (1/2) * (3*x**2 - 1)
-    #     # P_2_prime = lambda x: legendre_p(2, x, diff_n=1)[1]
-    #     P_2_prime = lambda x: 3*x
-
-    #     r_dot       = x[1]
-    #     p_dot       = x[3]**2 / x[0]**3 + SCALING_CONST * np.exp( - x[0] ) / x[0]**2 + \
-    #                     SCALING_CONST * np.exp( - x[0] ) / x[0] - \
-    #                     SCALING_CONST * 3 * alpha * MACH_NUM**2 * (1/2)*(3*np.cos(x[2])**2 - 1)/ x[0]**4
-    #     theta_dot   = x[3] / x[0]**2
-    #     l_dot       = SCALING_CONST * alpha * MACH_NUM**2 * -np.sin(x[2]) * 3 * np.cos(x[2]) / x[0]**3 
-
-    #     return [r_dot, p_dot, theta_dot, l_dot]
+    def _too_close(self, t, x): 
+        # stop solver when particles are distance r_stop apart
+        r_stop = 0.064
+        return x[0] - r_stop
+    _too_close.terminal = True
 
     def _generate_init_cond(self):
         # interptcl spacing
@@ -250,22 +222,24 @@ class Anisotropic_simulation(ys.Simulation):
             x_cart[2] = y
             x_cart[3] = y_dot
         '''
-        t = np.arange(0, duration, dt)
-        t_span = (t[0], t[-1])
+        t_desired = np.arange(0, duration, dt)
+        t_span = (t_desired[0], t_desired[-1])
 
         # generate initial conditions randomly if none entered
         if x0 is None:
             self._generate_init_cond()
         else:
             self.x0 = x0
-        x_clean = solve_ivp(self._EOM, t_span, self.x0, t_eval=t, **integrator_keywords).y.T
+        soln = solve_ivp(self._EOM, t_span, self.x0, t_eval=t_desired, events=self._too_close, **integrator_keywords)
+        x_clean = soln.y.T
+        t_actual = soln.t
 
         # save parameters as attributes
-        self.duration = duration
+        self.duration = t_actual[-1]
         self.dt = dt
 
         # save data as attributes
-        self.t = t
+        self.t = t_actual
         self.x = x_clean
 
         # convert data and initial conditions to cartesian 2D and save as attributes
@@ -278,12 +252,16 @@ class Anisotropic_simulation(ys.Simulation):
     
     def plot(self):
         # generate individual particle positions
-        r1, r1_dot, r2, r2_dot = self._generate_ptcl_coordinates(*self.x.T)
+        r1, r1_dot, r2, r2_dot = self._generate_ptcl_coordinates(*self.x_cart.T)
         # generate individual particle initial conditions
-        r1_0, r1_0_dot, r2_0, r2_0_dot = self._generate_ptcl_coordinates(*self.x0)
+        r1_0, r1_0_dot, r2_0, r2_0_dot = self._generate_ptcl_coordinates(*self.x0_cart)
         # plot trajectories and initial positions
         fig, axs = plt.subplots()
+        # square field of view
+        axs.set_aspect('equal', adjustable='datalim')
+        # use blue and green colors
         colors = ['tab:blue', 'tab:green']
+        # plot trajectories and starting positions
         axs.plot(*r1, label="particle 1", c=colors[0])
         axs.plot(*r1_0, 'o', label="particle 1 start", c=colors[0])
         axs.plot(*r2, label="particle 2", c=colors[1])
@@ -293,6 +271,8 @@ class Anisotropic_simulation(ys.Simulation):
         axs.set_xlabel("$x$")
         axs.set_ylabel("$y$")
         axs.legend(loc="upper left")
+        duration_str = f"duration = {self.duration:.3f}" + "$\omega_{pd}^{-1}$"
+        axs.text(0.72, 0.92, duration_str, transform=axs.transAxes)
 
         fig.tight_layout()
 
@@ -302,15 +282,12 @@ class Anisotropic_simulation(ys.Simulation):
 
     
 def main():
-    # create random number generator
-    seed = 103971
-    rng = np.random.default_rng(seed=seed)
-    # simulate accordingo to anisotropic Hamiltonian
-    duration = 3
-    sim = Anisotropic_simulation(rng=rng)
-    sim.simulate(duration)
-    sim.to_cart()
+    x0 = np.array([1, 0, np.pi, 0]) # format [r0, p0, theta0, l0]
+
+    sim = Anisotropic_simulation()
+    sim.simulate(0.4, x0=x0)
     sim.plot()
-    
+    plt.show()
+        
 if __name__ == '__main__':
     main()
